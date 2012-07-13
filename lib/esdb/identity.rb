bc0516da248c89b8854a9e53ae77719fcbdf4300
Marian Rudzynski <mr@impaled.org>
2012-07-13 21:09:33 +0200

    fixing previous commit oddity

--- lib/esdb/identity.rb
+++ lib/esdb/identity.rb
@@ -4,8 +4,10 @@ module ESDB
       get! unless @response
       hash = JSON.parse(@response)
 
+      Rails.logger.info hash.inspect
+
       # Only include stats for ourself.
-      hash['stats'] = hash['stats'][hash['id'].to_s]
+      hash['stats'] = hash['stats'][hash['id'].to_s] if hash['stats']
       hash
     end
   end
