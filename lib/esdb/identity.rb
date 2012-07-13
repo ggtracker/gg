b86e5a1392220736679ff160320460e3af1b8ba4
Marian Rudzynski <mr@impaled.org>
2012-07-13 21:05:23 +0200

    on Identity, only include stats for self

--- lib/esdb/identity.rb
+++ lib/esdb/identity.rb
@@ -1,4 +1,12 @@
 module ESDB
   class Identity < ESDB::Resource
+    def to_hash
+      get! unless @response
+      hash = JSON.parse(@response)
+
+      # Only include stats for ourself.
+      hash['stats'] = hash['stats'][hash['id'].to_s]
+      hash
+    end
   end
 end
