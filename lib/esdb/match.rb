6af106302ecaaf83e90ced95c07d5f4868cca436
David Joerg <dsjoerg@gmail.com>
2012-09-21 13:51:37 -0400

    added chart_data() to permit charting of a match's per-minute data

--- lib/esdb/match.rb
+++ lib/esdb/match.rb
@@ -3,5 +3,24 @@ module ESDB
     def duration_minutes
       return (duration_seconds / 60.0).round
     end
+
+    # where does this function belong?
+    #
+    # MR says "[if it] deals directly with data on the model, it goes in the model"
+    #
+    # and yet this feels suspiciously like view logic.
+    #
+    def chart_data(measure)
+      entities.collect {|entity|
+        extracted_measures = []
+        entity["minutes"].each {|minute, measures|
+          extracted_measures[minute.to_i] = measures[measure].to_f
+        }
+        {:name => entity["identity"]["name"],
+         :data => extracted_measures,
+         :color => "#" + entity["color"],
+        }
+      }.to_json
+    end
   end
 end
