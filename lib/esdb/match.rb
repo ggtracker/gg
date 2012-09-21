64e5e92383fc3014db4d7ddc1c2bff04df8e5e42
David Joerg <dsjoerg@gmail.com>
2012-09-21 14:34:56 -0400

    do smoothing on chart data

--- lib/esdb/match.rb
+++ lib/esdb/match.rb
@@ -4,20 +4,39 @@ module ESDB
       return (duration_seconds / 60.0).round
     end
 
+    # this function doesnt belong here. where does it belong?
+    def smooth(indata, window_size)
+      window = []
+      result = []
+      runningavg = 0
+      for elm in indata
+        elm = 0 if elm.nil?
+        runningavg += elm
+        window.push(elm)
+        if window.length > window_size
+          removedelm = window.shift
+          runningavg -= removedelm
+        end
+        windowavg = runningavg.to_f / window.length
+        result.push(windowavg.round(2))
+      end
+      return result
+    end
+
     # where does this function belong?
     #
     # MR says "[if it] deals directly with data on the model, it goes in the model"
     #
     # and yet this feels suspiciously like view logic.
     #
-    def chart_data(measure)
+    def chart_data(measure, smoothing_window)
       entities.collect {|entity|
         extracted_measures = []
         entity["minutes"].each {|minute, measures|
-          extracted_measures[minute.to_i] = measures[measure].to_f
+          extracted_measures[minute.to_i - 1] = measures[measure].to_f unless minute == "0"
         }
         {:name => entity["identity"]["name"],
-         :data => extracted_measures,
+         :data => smooth(extracted_measures, smoothing_window),
          :color => "#" + entity["color"],
         }
       }.to_json
